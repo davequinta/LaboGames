1be23830faeb3edfe44c4deef0ed096805abc276
hmmatus <manriquematus1@gmail.com>
2017-06-23 00:09:05 -0600

    Commit changes

--- app/src/main/java/com/quintanilla00025815/noticias/noticias/NoticiasPrincipal.java
+++ app/src/main/java/com/quintanilla00025815/noticias/noticias/NoticiasPrincipal.java
@@ -1,23 +1,46 @@
 package com.quintanilla00025815.noticias.noticias;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.quintanilla00025815.labogames.R;
+import com.quintanilla00025815.labogames.TabbedGames;
 
 import java.util.ArrayList;
 
-public class NoticiasPrincipal extends AppCompatActivity {
-    String url="http://192.168.1.7/WebServer/noticias/noticias.php";
+public class NoticiasPrincipal extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+    String url="http://192.168.0.19/WebServer/noticias.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_noticias);
+        setContentView(R.layout.activity_news_menu);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         final RecyclerView rv= (RecyclerView) findViewById(R.id.mRecycler);
         StaggeredGridLayoutManager StaggManager = new StaggeredGridLayoutManager(2, GridLayoutManager.VERTICAL);
         rv.setLayoutManager(StaggManager);
@@ -41,5 +64,71 @@ public class NoticiasPrincipal extends AppCompatActivity {
         }
         * */
         rv.setAdapter(new StaggeredGridLayoutAdapter(this, Noticias));
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.news) {
+
+
+        } else if (id == R.id.lol) {
+            String game="League%20of%20Legends";
+            Intent intent= new Intent(this,TabbedGames.class);
+            intent.putExtra("namegame",game);
+            startActivity(intent);
+        } else if (id == R.id.dota) {
+            String game="DOTA";
+            Intent intent= new Intent(this,TabbedGames.class);
+            intent.putExtra("namegame",game);
+            startActivity(intent);
+        } else if (id == R.id.csgo) {
+            String game="Counter Strike Go";
+            Intent intent= new Intent(this,TabbedGames.class);
+            intent.putExtra("namegame",game);
+            startActivity(intent);
+        } else if (id == R.id.action_settings) {
+
+        } else if (id == R.id.favorites) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
